27c49065673246ac79c784724d25664b244b3de5
Jacob Eichler <jeichler7@gmail.com>
2022-08-31 16:21:47 -0600

    fixed typo in server file

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/styels.css"))
+    res.sendFile(path.join(__dirname, "../client/styles.css"))
 })
 
 app.get('/js', (req, res) => {
