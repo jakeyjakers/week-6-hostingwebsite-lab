187d0d70f310913fe47dc29bd69c1004071cbd1d
Jacob Eichler <jeichler7@gmail.com>
2022-08-31 14:00:26 -0600

    Inital commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+///// endpoints  ///////
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"))
+})
+
+
+
+const port = process.env.PORT || 4066
+
+app.listen(port, () => console.log(`Running on port ${port}`))
