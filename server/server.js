5c6dea92299069a71fa0c08561a2a205f88eba30
Jacob Eichler <jeichler7@gmail.com>
2022-08-31 14:55:32 -0600

    updates

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/styels.css"))
+})
 
+app.get('/js', (req, res) => {
+    req.sendFile(path.join(__dirname, "../client/client.js"))
+})
 
 const port = process.env.PORT || 4066
 
